20f8aad1f905f4fe4819160814d91d6938e7f581
Sean Ockert <seanockert@gmail.com>
2015-12-26 22:18:00 +1000

    Made onscroll event more performant
    and added Github link

--- js/plugins/scroll-events.js
+++ js/plugins/scroll-events.js
@@ -1,28 +1,29 @@
 /* scrollEvents function attaches a class="scrolled" to the body when scrolling the page so you can hook into that class
 * Also looks for a navigation list named #main-nav and adds class="active" to the nav item that is currently scrolled to
-* Sean Ockert 2014
-* v1
+* Sean Ockert 2015
+* v1.1
 */
 
 // Generic onResize listener - replace if using a framework
 var addEvent = function(elem, type, eventHandle) {
-    if (elem == null || typeof(elem) == 'undefined') return;
-    if ( elem.addEventListener ) {
-        elem.addEventListener( type, eventHandle, false );
-    } else if ( elem.attachEvent ) {
-        elem.attachEvent( "on" + type, eventHandle );
-    } else {
-        elem["on"+type]=eventHandle;
-    }
+  if (elem == null || typeof(elem) == 'undefined') return;
+  if ( elem.addEventListener ) {
+      elem.addEventListener( type, eventHandle, false );
+  } else if ( elem.attachEvent ) {
+      elem.attachEvent( "on" + type, eventHandle );
+  } else {
+      elem["on"+type]=eventHandle;
+  }
 };
 
 // scrollEvents function autoloads
-window.scrollEvents = (function(){
+window.scrollEvents = (function() {
 
   var navContainer = document.getElementById('main-nav');
   var anchors = [];
   var hasNav = false;
   var anchorCount = 0;
+  var didScroll = false;
 
   var body = document.querySelector('body'),
     shrinkOn = 80; // Pixels from the top of the page
@@ -49,35 +50,43 @@ window.scrollEvents = (function(){
 
   calcAnchorDist();
 
-  // Listen for scroll events and attach a class
-  addEvent(window, 'scroll', function(e) {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+  window.onscroll = scrollEvent;
 
-    // Apply a class 'scrolled' to body when starting to scroll page
-    if (distanceY > shrinkOn) {
-      classie.add(body,'scrolled');
-    } else {
-      if (classie.has(body,'scrolled')) {
-        classie.remove(body,'scrolled');
+  function scrollEvent() {
+      didScroll = true;
+  }
+
+  setInterval(function() {
+    if (didScroll) {
+      didScroll = false;
+
+      var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+
+      // Apply a class 'scrolled' to body when starting to scroll page
+      if (distanceY > shrinkOn) {
+        classie.add(body,'scrolled');
+      } else {
+        if (classie.has(body,'scrolled')) {
+          classie.remove(body,'scrolled');
+        }
       }
-    }
 
-    // For checking the active nav item and giving it an 'active' class. Only check if #main-nav element exists
-    if (hasNav) {
-      var active = 0;
-      for (i = 0; i < anchorCount-1; ++i) {
-        var anchor = anchors[i];
-        var menuItem = navContainer.children[i];
-        classie.remove(menuItem,'active');
+      // For checking the active nav item and giving it an 'active' class. Only check if #main-nav element exists
+      if (hasNav) {
+        var active = 0;
+        for (i = 0; i < anchorCount-1; ++i) {
+          var anchor = anchors[i];
+          var menuItem = navContainer.children[i];
+          classie.remove(menuItem, 'active');
 
-        if (distanceY >= anchor && distanceY < anchors[i+1]) {
-          active = i;
-          classie.add(menuItem,'active');
+          if (distanceY >= anchor && distanceY < anchors[i+1]) {
+            active = i;
+            classie.add(menuItem, 'active');
+          }
         }
       }
     }
-
-  });
+  }, 100);
 
   // Listen for document resize
   addEvent(window, 'resize', function(e) {
